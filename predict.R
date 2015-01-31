77863a5f5491350511eb430333de77c9af45eb8e
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2015-01-31 02:37:37 -0300

    New daily forecast with horizon = 60

--- predict.R
+++ predict.R
@@ -6,36 +6,33 @@ if (file.exists('local_config.R')){
 library(forecast)
 library(RPostgreSQL)
 library(RJSONIO)
-add.months= function(date,n) seq(date, by = paste (n, "months"), length = 2)[2]
 
 drv <- dbDriver("PostgreSQL")
 
 con <- dbConnect(drv, dbname="bluelytics")
 dbSendQuery(con,"SET datestyle TO iso")
-rs <- dbSendQuery(con, "select avg(value_sell) as value, date_trunc('day', date) as date from dolar_blue_dolarblue where source_id <> 'oficial' group by date_trunc('day', date) order by date;")
+rs <- dbSendQuery(con, "select avg(value_sell) as value, date_trunc('day', date) as date from dolar_blue_dolarblue where source_id <> 'oficial' and date >= '2013-01-01' group by date_trunc('day', date) order by date;")
 base_data <- fetch(rs,n=-1)
 
 dbDisconnect(con)
 dbUnloadDriver(drv)
 
-agg <- setNames(aggregate(base_data$value_sell, by=list(as.Date(base_data$date)), FUN=mean), c('date', 'x'))
+agg <- setNames(aggregate(base_data$value, by=list(as.Date(base_data$date)), FUN=mean), c('date', 'x'))
 
 days <- data.frame ( date = as.Date(seq.POSIXt(ymd(min(agg$date)), ymd(max(agg$date)), by = "1 day")))
 
 final <- na.locf(merge(days, agg, by="date", all.x=TRUE))
 final$x <- as.numeric(final$x)
 
+basedate = min(ymd(final$date))
 
-start_date <- base_data$date[1]
-start_year <- as.numeric(strftime(start_date, format="%Y"))
-start_month <- as.numeric(strftime(start_date, format="%m"))
+fts <- ts(final$x, start=c(year(basedate), yday(basedate)), frequency=365)
 
-bts <- ts(base_data$value, start=c(start_year, start_month), frequency=12)
+fit_arima <- Arima(fts, order=c(2,1,2), include.drift=TRUE,  method="ML")
 
-fit_arima <- Arima(bts, order=c(0,1,1), seasonal=c(0,1,0))
+horizon <- 60
 
-
-forecasted <- forecast(fit_arima, h=12)
+forecasted <- forecast(fit_arima, h=horizon)
 
 lower_80 <- forecasted$lower[,1]
 lower_95 <- forecasted$lower[,2]
@@ -45,8 +42,8 @@ mean <- as.numeric(forecasted$mean)
 
 data_forecasted = data.frame(lower_95, lower_80, mean, upper_80, upper_95)
 
-dates_history <- seq(start_date, by='month', length.out=nrow(base_data))
-dates_forecasted <- seq(add.months(tail(dates_history, n=1), 1), by='month', length.out=12)
+dates_history <- seq(basedate, by='day', length.out=nrow(days))
+dates_forecasted <- seq(as.Date(tail(dates_history, n=1))+1, by='day', length.out=horizon)
 
 export_history <- strftime(dates_history, '%m/%Y')
 export_forecasted <- strftime(dates_forecasted, '%m/%Y')
